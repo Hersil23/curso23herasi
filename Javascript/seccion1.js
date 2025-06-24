3caf935d7452d1d3d21dad5ae0befef1da6c0032
Herasi Silva <herasidesweb@gmail.com>
2025-06-24 19:09:14 -0400

    1

---
+++ Javascript/seccion1.js
@@ -0,0 +1,28 @@
+//Esto es un comentario de una línea
+/*
+  Comentario de multiples
+  líneas
+*/
+
+//Variables
+/*
+  Las variables son espacios de memoria que almacenan un valor.
+*/
+// Declaración de una variable
+let nombre = "Juan"; // Variable
+console.log(nombre); // Imprime "Juan"
+// Reasignación de una variable
+nombre = "Pedro"; // Reasignación
+console.log(nombre); // Imprime "Pedro"
+
+// Tipos de datos
+let number = 42; // Número
+let float = 3.14; // Número de punto flotante
+let string = "Hola, mundo"; // Cadena de texto
+let boolean = true; // Booleano (true/false)
+let array = ['pera', 'manzana', 'naranja']; // Arreglo
+
+let suma = 5 + 3; // Suma
+let resta = "10" - 2; // Resta
+
+console.log(suma); // Imprime 8
