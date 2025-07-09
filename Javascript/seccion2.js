1953f71ebc8d5389bbf460896c431b1770dc8d54
Herasi Silva <herasidesweb@gmail.com>
2025-07-09 19:47:34 -0400

    Update seccion2.js

--- Javascript/seccion2.js
+++ Javascript/seccion2.js
@@ -449,3 +449,77 @@ frutasArray.unshift("fresa");   // Agrega "fresa" al inicio del array
 frutasArray.splice(2, 1, "Kiwi"); // Elimina el elemento en el índice 2 y agrega "kiwi"
 let nuevoArray = frutasArray.slice(1, 4); // Crea un nuevo array con los elementos del índice 1 al 3
 
+
+/*
+  Arrow function o funciones anonimas
+  Las arrow functions son una forma concisa de escribir funciones anonimas en JavaScript.
+*/
+
+let sumar = (num1, num2) => {
+  return num1 + num2;
+}
+
+sumar(5, 10); // Esto devuelve 15
+sumar(6, 20); // Esto devuelve 26
+
+/*
+  Los callbacks
+  Los callbacks son funciones que se pasan como argumentos a otras funciones y se ejecutan después de que se completa una tarea.
+*/
+function registro(nombre, callback) {
+  if (callback(nombre)) {
+    console.log("Registro exitoso para: " + nombre);
+  }
+}
+
+/*
+let funcion = validarNombre;
+funcion("juan")
+registro("juan", validarNombre); // Esto llama a la función validarNombre como callback
+*/
+//validarNombre("juan"); // Esto valida el nombre y muestra mensajes de error si es necesario
+//validarNombre // La funcion simplemente se pasa como argumento sin ejecutarla
+
+console.log(validarNombre);
+
+function validarNombre(nombre) {
+  if (typeof nombre !== "string") {
+    console.log("El nombre debe ser una cadena de texto.");
+    return false;
+  } else if (nombre.length < 3) {
+    console.log("El nombre debe tener al menos 3 caracteres.");
+    return false;
+  } else if (nombre.length > 20) {
+    console.log("El nombre no puede tener más de 20 caracteres.");
+    return false;
+  }
+  return true;
+}
+
+/*
+  1. Crea una función que reciba un texto o parrafo y verifique si contiene alguna las siguientes malas palabras:
+  - "tonto"
+  - "chispas"
+  - "recorcholis"
+  - "rayos"
+  - "caracoles"
+  - "cielos"
+  Si el texto contiene alguna de estas palabras, sustituye por "****" y retorna el texto modificado.
+  Si no contiene ninguna, retorna el texto original.
+
+  2. Según el siguiente array de objetos
+  let personas = [
+    { nombre: "Juan", edad: 25, pais: "España", sexo: "masculino" },
+    { nombre: "María", edad: 30, pais: "México", sexo: "femenino" },
+    { nombre: "Pedro", edad: 22, pais: "Argentina", sexo: "masculino" },
+    { nombre: "Ana", edad: 28, pais: "Chile", sexo: "femenino" },
+    { nombre: "Luis", edad: 35, pais: "España", sexo: "masculino" },
+  ];
+
+  Crea un código que me haga un filtro del array creando un nuevo array que contenga solo
+  las personas que sean mayores de 25 años.
+
+  Luego haz un código que me cree un nuevo array con las personas sean de españa y sean hombres
+
+  Fecha de entrega: 11/07/25
+*/
