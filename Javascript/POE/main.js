07beb51778c57dc45062f30cf970fe80a99dbbd2
Herasi Silva <herasidesweb@gmail.com>
2025-07-21 19:56:00 -0400

    1

---
+++ Javascript/POE/main.js
@@ -0,0 +1,13 @@
+// const labels = document.querySelectorAll("label");
+// for (const label of labels) {
+//   label.textContent = "Adios";
+// }
+
+// const btnTouchMe = document.getElementById("btnTouchMe");
+// btnTouchMe.textContent = "Hola";
+
+// const cosita = document.getElementsByClassName("cosita");
+// cosita[0].textContent = "Soy cosita";
+
+const label1 = document.querySelector("label[name='cosita1']");
+label1.textContent = "Soy cosita 1 y me han cambiado";
