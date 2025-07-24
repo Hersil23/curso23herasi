e1e3014c188e539869d1f02bd5a26bc771e78961
Herasi Silva <herasidesweb@gmail.com>
2025-07-24 14:50:27 -0400

    6

--- Javascript/ejercicios/ejercicios_poe.js
+++ Javascript/ejercicios/ejercicios_poe.js
@@ -1,5 +1,6 @@
 const ChangeText = document.querySelector("#ChangeTextboton");
 const ChangeColor = document.querySelector("#ChangeColorboton");
+const lista = document.querySelector("#lista");
 
 // boton que cambia su texto
 ChangeText.addEventListener("click", () => {
@@ -13,4 +14,11 @@ ChangeColor.addEventListener("click", () => {
   ChangeColor.style.color = "#000";
   ChangeColor.textContent = "Me han cambiado el color";
   ChangeColor.style.border = "2px solid #000";
+});
+
+// boton que añade un elemento a la lista
+addElement.addEventListener("click", () => {
+  const newElement = document.createElement("li");
+  newElement.textContent = "Soy el nuevo elemento agegado a la lista";
+  lista.appendChild(newElement);
 });
